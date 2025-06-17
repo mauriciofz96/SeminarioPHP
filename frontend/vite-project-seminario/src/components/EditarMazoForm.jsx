ece947462c64a4548a0db271a9fc8fd1c9295f83
FrancoMartin03 <francomartin08@hotmail.com>
2025-06-17 18:30:13 -0300

    Create EditarMazoForm.jsx
    Formulario para editar nombre de mazo

---
+++ frontend/vite-project-seminario/src/components/EditarMazoForm.jsx
@@ -0,0 +1,19 @@
+
+function EditarMazoForm({nuevoNombre, setNuevoNombre, onGuardar, onCancel}){
+    return (
+        <form className="editar-form"
+            onSubmit={e=>{
+            e.preventDefault();
+            onGuardar();
+        }}>
+            <input
+              type="text"
+              value={nuevoNombre}
+              onChange={e => setNuevoNombre(e.target.value)}
+            />
+            <button type="submit">Guardar</button>
+            <button type="button" onClick={onCancel}>Cancelar</button>
+        </form>
+    )
+}
+export default EditarMazoForm;

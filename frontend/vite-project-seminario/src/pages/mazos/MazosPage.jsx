2c7685206d5b28919427f50c43c5c8361ef3e703
FrancoMartin03 <francomartin08@hotmail.com>
2025-06-17 18:32:14 -0300

    Nueva carpeta mazos + Nueva Pagina de mazos
    Solo está la funcion de editar nombre de momento

---
+++ frontend/vite-project-seminario/src/pages/mazos/MazosPage.jsx
@@ -0,0 +1,99 @@
+import '../../assets/styles/MazosPage.css';
+import EditarMazoForm from '../../components/EditarMazoForm';
+import {useEffect, useState} from 'react';
+import { getMazos } from "../../services/apiService";
+import { editarMazo } from '../../services/apiService';
+import { useNavigate } from 'react-router-dom';
+
+function MazosPage(){
+    const navigate = useNavigate();
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+
+    if (!id || !token) {
+      navigate('/login')
+    }
+
+    
+    const [mazos, setMazos] = useState([]);
+    const[mazoSeleccionado, setMazoSeleccionado] = useState(null);
+    //obtengo los mazos
+    async function fetchMazos(){
+        try{
+            const response = await getMazos(id,token);
+            setMazos(response.data);
+        }catch(error){
+            console.error(error);
+        }
+    }
+    useEffect(()=>{
+        fetchMazos();
+    }, [id,token]);
+
+    const [nuevoNombre, setNuevoNombre] = useState('');
+    const [editando, setEditando] = useState(null);
+
+    const handleClickEditar = (mazo)=>{
+         setNuevoNombre(mazo.nombre);
+         setEditando(mazo.id);
+    }
+
+    const handleGuardar = async () => {
+        try{
+            const response= await editarMazo(editando, nuevoNombre, token);
+            if(response.status === 200){
+              //refrescar la lista de mazos
+              setEditando(null);
+              fetchMazos();
+            }
+
+        }catch(error){
+            console.error('Error al cambiar el nombre del mazo:', error);
+        }
+    }
+    
+
+    const [cartas, setCartas] = useState([]);
+    
+
+    return(
+        <div>
+            <h1>Mazos disponibles</h1>
+            <ul>
+                {mazos && mazos.length>0 ? (
+                    mazos.map((mazo) => (
+                        <li
+                        key={mazo.id} 
+                        onClick={()=> {
+                            setMazoSeleccionado(mazo.id)
+                            setEditando(null);
+                        }}
+                        className={`mazo${mazoSeleccionado === mazo.id ? ' seleccionado' : ''}`}> 
+                        <span >{mazo.nombre}</span>
+                       {mazoSeleccionado ==mazo.id &&
+                       <div className="mazo-opciones">
+                          <button>Ver Mazo</button>
+                          <button> Eliminar</button>
+                          <button onClick={e => {e.stopPropagation(); handleClickEditar(mazo)}}> Editar</button>
+                          <button> Jugar</button>
+                        </div>}
+                        {editando === mazo.id &&(
+                            <EditarMazoForm
+                            nuevoNombre={nuevoNombre}
+                            setNuevoNombre={setNuevoNombre}
+                            onGuardar={handleGuardar}
+                            onCancel={() => setEditando(null)}
+                            />
+                        )}
+
+                    </li>                 
+                    ))
+                ): (
+                    <p>No tienes mazos disponibles</p>
+                )}
+            </ul>
+            
+        </div>
+    );
+}
+export default MazosPage;

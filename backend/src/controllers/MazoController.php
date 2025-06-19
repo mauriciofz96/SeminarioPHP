1e680f0b45080551fda79ccee57bfd7035ce2d1d
FrancoMartin03 <francomartin08@hotmail.com>
2025-06-19 10:38:19 -0300

    Update MazoController.php
    faltaba el use App/models/carta

--- backend/src/controllers/MazoController.php
+++ backend/src/controllers/MazoController.php
@@ -4,6 +4,7 @@ namespace App\controllers;
 
 require_once __DIR__ . '/../models/Mazo.php';
 
+use App\models\Carta;
 use App\models\Mazo;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;

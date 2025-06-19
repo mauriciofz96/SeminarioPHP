8de6c69c4bc5fcd7c19312f0b163ae913c9fef46
Hernan Neves <hernan7.hrn@gmail.com>
2025-06-19 17:11:34 -0300

    Update RegistroExitoso.jsx

--- frontend/vite-project-seminario/src/pages/registro/RegistroExitoso.jsx
+++ frontend/vite-project-seminario/src/pages/registro/RegistroExitoso.jsx
@@ -1,14 +1,13 @@
 import { useLocation } from 'react-router-dom'
-import '../../assets/styles/RegistroPage.css'
 
 function RegistroExitoso() {
   const location = useLocation()
   const mensaje = location.state?.mensaje || 'Registro completado con éxito.'
 
   return (
-    <div>
-      <h2>¡Éxito!</h2>
-      <p>{mensaje}</p>
+    <div className="max-w-md mx-auto mt-10 bg-green-100 rounded-xl shadow-lg p-8 border-4 border-green-400 text-center">
+      <h2 className="text-3xl font-extrabold text-green-700 mb-4 drop-shadow-lg">¡Éxito!</h2>
+      <p className="text-lg font-semibold">{mensaje}</p>
     </div>
   )
 }

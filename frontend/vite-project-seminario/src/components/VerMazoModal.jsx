a564917c974bbc2dc2247f3c588626155892294c
FrancoMartin03 <francomartin08@hotmail.com>
2025-06-20 18:40:15 -0300

    Update VerMazoModal.jsx
    Modal estilado

--- frontend/vite-project-seminario/src/components/VerMazoModal.jsx
+++ frontend/vite-project-seminario/src/components/VerMazoModal.jsx
@@ -3,24 +3,26 @@
 function VerMazoModal({mazo,cartas,cargando,onClose}){
     const cartasArray = Array.isArray(cartas) ? cartas : Object.values(cartas || {});
     return(
-        <div className="modal-fondo">
-            <div className="modal-contenido">
-                <h2>{mazo.nombre}</h2>
-                <button className="close-button" onClick={onClose}>X</button>
+        <div className="fixed inset-0 shadow-black flex items-center justify-center z-[]">
+            <div className="relative bg-gradient-to-r from-blue-800 to-blue-900 p-6 rounded-[10px] min-w-[300px] max-w-[900px] shadow-lg ">
+                <h2 className="mt-1 text-white font-bold font-sans text-2xl text-center">{mazo.nombre}</h2>
+                <button className="absolute top-3 right-3 bg-red-600 text-white rounded-full w-8 h-8 text-lg flex items-center justify-center hover:bg-red-700 transition" onClick={onClose}>X</button>
                 {cargando || !cartas ?(<p>Cargando cartas...</p>):(
-                <ul className="cartas-lista">
+                <ul className="flex flex-row gap-2 list-none p-0">
                         {cartasArray.map((carta) => (
-                         <div className="carta" key={carta.id}>
+                         <li className="flex flex-col gap-2.5 list-none p-0" key={carta.id}>
+                            <div className="bg-[#ea0c0c] rounded-xl p-3 flex flex-col items-center shadow-md">
                              <img src={`/images/${carta.nombre.toLowerCase().replace(/\s+/g, '') + '.png'}`}
                              alt={carta.nombre}
-                             className="carta-imagen" />
-                             <div className="carta-info">
-                                 <h4>{carta.nombre}</h4>
-                                 <p>Ataque: {carta.ataque}</p>
-                                 <p>Tipo: {carta.atributo}</p>
-                                 <p>Habilidad: {carta.ataque_nombre}</p>
+                             className="w-36 h-auto object-contain mb-2.5 rounded-lg border-4 border-yellow-400" />
+                             <div className="mt-1 text-white font-bold font-sans text-center">
+                                 <h4 className="my-1 text-xl">{carta.nombre}</h4>
+                                 <p className="my-0.5 text-base">Ataque: {carta.ataque}</p>
+                                 <p className="my-0.5 text-base">Tipo: {carta.atributo}</p>
+                                 <p className="my-0.5 text-base">Habilidad: {carta.ataque_nombre}</p>
                              </div>
-                         </div>
+                             </div>
+                         </li>
                         ))}
                 </ul>
                 )}
@@ -29,3 +31,4 @@ function VerMazoModal({mazo,cartas,cargando,onClose}){
     )
 }
 export default VerMazoModal;
+

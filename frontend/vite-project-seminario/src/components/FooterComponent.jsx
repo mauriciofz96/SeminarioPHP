1a8fbd76d016708b36366f5a7f63257e8e4e041a
Mauricio Flammini Zuccheri <mauriciofz18@gmail.com>
2025-06-20 15:51:35 -0300

    update

--- frontend/vite-project-seminario/src/components/FooterComponent.jsx
+++ frontend/vite-project-seminario/src/components/FooterComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function FooterComponent() {
   return (
     <footer className="bg-gray-100 text-center py-4 text-sm text-gray-600 mt-10">
-     Integrantes: Flaminni Zuccheri Mauricio, Martín Franco, Rodrigues Neves Hernan © 2025
+     Integrantes: Flammini Zuccheri Mauricio, Martín Franco, Rodrigues Neves Hernan © 2025
     </footer>
   );
 }

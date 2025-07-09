62408c7a08ab7365b2bbfdea2a45ff1f25b43078
FrancoMartin03 <francomartin08@hotmail.com>
2025-07-09 16:39:58 -0300

    Update apiService.js
    Actualizado para utilizar la config de axios con nuestra url (localhost:8000)

--- frontend/vite-project-seminario/src/services/apiService.js
+++ frontend/vite-project-seminario/src/services/apiService.js
@@ -1,85 +1,72 @@
-import { api } from "../api/api";
+import {api} from "../api/api.js";
 
-// /services/apiService.js
-import axios from 'axios'
 
-// Obtener estadísticas de usuarios
 export function getEstadisticas() {
-  return axios.get('/api/estadisticas')
+  return api.get('/estadisticas')
 }
- 
-// Verificar si un nombre de usuario está disponible
+
 export function verificarUsuarioDisponible(usuario) {
-  return axios.get(`/api/verificar-usuario/${usuario}`)
+  return api.get(`/verificar-usuario/${usuario}`)
 }
 
-// Registrar un nuevo usuario
+
 export function registrarUsuario(data) {
-  return axios.post('/api/registro', data)
+  return api.post('/registro', data)
 }
 
-// iniciar sesion
+
 export function postLogin(credentials){
-  return axios.post('/api/login', credentials)
+  return api.post('/login', credentials)
 }
 
-// obtener datos de un usuario logeado
-export function obtenerUsuario(id, token) {
-  return axios.get(`/api/usuarios/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-}
 
-// editar datos de un usuario logeado
 export function actualizarUsuario(usuario, datosActualizados, token) {
-  return axios.put(`/api/usuarios/${usuario}`, datosActualizados, {
+  return api.put(`/usuarios/${usuario}`, datosActualizados, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
 }
-//obetener los mazos de un usuario
+
 export function getMazos(id, token) {
-  return axios.get(`/api/usuarios/${id}/mazos`, {
+  return api.get(`/usuarios/${id}/mazos`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
 }
 
-//editar nombre de mazo
+
 export function editarMazo(mazo, nombreNuevo,token){
-  return axios.put(`/api/mazos/${mazo}`,{nombre: nombreNuevo}, {
+  return api.put(`/mazos/${mazo}`,{nombre: nombreNuevo}, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
 }
 
-//Obtener cartas de un mazo
+
 export function getCartasEnMazo(mazo,token) {
-  return axios.get(`/api/mazos/${mazo}/cartas`,{
+  return api.get(`/mazos/${mazo}/cartas`,{
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
 }
 
-// Obtener cartas con filtro por atributo y/o nombre
+
 export function getCartas({ atributo = '', nombre = '' }) {
   const params = new URLSearchParams();
   if (atributo) params.append('atributo', atributo);
   if (nombre) params.append('nombre', nombre);
 
-  return axios.get(`/api/cartas?${params.toString()}`);
+  return api.get(`/cartas?${params.toString()}`);
 }
 
 
-// Crear un nuevo mazo
+
 export function crearMazo(nombre, cartas, token) {
-  return axios.post('/api/mazos', {
+  return api.post('/mazos', {
     nombre,
     cartas
   }, {
@@ -90,7 +77,7 @@ export function crearMazo(nombre, cartas, token) {
 }
 
 export function borrarMazo(mazo, token){
-  return axios.delete(`/api/mazos/${mazo}`,{
+  return api.delete(`/mazos/${mazo}`,{
     headers: {
       Authorization: `Bearer ${token}`
     }
@@ -98,11 +85,9 @@ export function borrarMazo(mazo, token){
   )
 }
 
-//NUEVO
 
-//crear partida
 export function crearPartida(mazo_id,token){
-  return axios.post(`/api/partidas`,{
+  return api.post(`/partidas`,{
     mazo_id
   },{
     headers: {
@@ -111,9 +96,9 @@ export function crearPartida(mazo_id,token){
   })
 }
 
-//jugada
+
 export function realizarJugada(token,carta_id,partida_id,usuario_id){
-  return axios.post(`/api/jugadas`,{
+  return api.post(`/jugadas`,{
     carta_id,
     partida_id,
     usuario_id,
@@ -124,9 +109,9 @@ export function realizarJugada(token,carta_id,partida_id,usuario_id){
   })
 }
 
-//listar cartas en mano de la partida actual
+
 export function listarCartasEnMano(usuario,partida,token){
-  return axios.get(`/api/usuarios/${usuario}/partidas/${partida}/cartas`,{
+  return api.get(`/usuarios/${usuario}/partidas/${partida}/cartas`,{
     headers: {
       Authorization: `Bearer ${token}`
     }
